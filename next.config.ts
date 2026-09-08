cb4a5a81113c4a445c6b28fba1736b7f4a60a2a2
Gowtham <gowthamtg77@gmail.com>
2026-09-08 17:22:12 +0530

    Second commit for test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
+// import type { NextConfig } from "next";
+//
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true,
+// };
+//
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
+const backendApiUrl = process.env.BACKEND_API_URL;
+
+if (!backendApiUrl) {
+  throw new Error(
+    "BACKEND_API_URL is not configured. Please set it in your environment variables.",
+  );
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/backend-api/:path*",
+        destination: `${backendApiUrl}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
